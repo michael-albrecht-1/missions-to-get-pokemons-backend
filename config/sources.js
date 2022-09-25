eef353cb47df373af39cbfcb3c7b4a491d912dbe
albrecht <albrecht.michael@hotmail.fr>
2022-09-25 20:08:39 +0200

    #

--- config/sources.js
+++
@@ -1,9 +0,0 @@
-const SOURCES = {
-  mongoLocal: "mongodb://localhost:27017/pokedex",
-  mongoTest:
-    "mongodb+srv://mike34a:Yk9SEgHHQFo5fYVz@cluster0.1ypbamj.mongodb.net/test",
-  mongoProd:
-    "mongodb+srv://mike34a:Yk9SEgHHQFo5fYVz@cluster0.1ypbamj.mongodb.net/missionPokemon",
-};
-
-module.exports = SOURCES;
