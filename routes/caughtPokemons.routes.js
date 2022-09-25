0af8de3b6a98074b36d84fa2d809232052fbfaff
albrecht <albrecht.michael@hotmail.fr>
2022-09-25 20:57:30 +0200

    fix vercel ci

--- routes/caughtPokemons.routes.js
+++ routes/caughtPokemons.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const caughtPokemonsController = require("../controllers/caughtPokemons.controller");
+const caughtPokemonsController = require("../controllers/CaughtPokemons.controller");
 
 router.post("/", caughtPokemonsController.addCaughtPokemon);
 router.get("/", caughtPokemonsController.searchCaughtPokemons);
--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(
-  process.env.SOURCE_MONGO_PROD,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false,
-  },
-  (err) => {
-    if (!err) console.log("Mongodb local connected");
-    else console.log("Connection Mongodb local error" + err);
-  }
-);
+require("./config/dbConfig");
 const caughtPokemonsRoutes = require("./routes/caughtPokemons.routes");
 const missionsRoutes = require("./routes/missions.routes");
 const bodyParser = require("body-parser");
