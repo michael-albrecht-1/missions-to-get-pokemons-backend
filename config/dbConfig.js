ac11f3a590ce946caba259f4021e7867be1b4465
Simon Boeuf <simon.boeuf@gmail.com>
2022-10-31 21:57:21 +0100

    :rocket: chore(env): remove environment dependencies from sources

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,20 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.SOURCE_MONGO_LOCAL,
+  process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     autoIndex: false,
   },
   (err) => {
-    if (!err) console.log("Mongodb local connected");
-    else console.log("Connection Mongodb local error" + err);
+    if (!err) console.log("Mongodb connected");
+    else console.log("Connection Mongodb error" + err);
   }
 );
-
-/*
-SOURCE_MONGO_PROD;
-SOURCE_MONGO_TEST;
-SOURCE_MONGO_LOCAL;
-*/
