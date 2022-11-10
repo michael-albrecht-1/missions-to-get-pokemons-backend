d6146b68d9be068afc1437466bd964fde58aa1c0
michael.albrecht <albrecht.michael@hotmail.com>
2022-11-10 22:41:06 +0100

    searchPokemons sorted by number

--- src/controllers/Pokemons.controller.ts
+++ src/controllers/Pokemons.controller.ts
@@ -2,24 +2,10 @@ import { Handler } from 'express';
 import { PokemonsModel } from '../models/pokemonModel';
 const mongoose = require('mongoose');
 
-export const searchPokemons: Handler = (req, res) => {
-  PokemonsModel.find((err, docs) => {
-    if (!err) {
-      res.json(
-        docs
-          .map((doc) => {
-            return {
-              id: doc.id,
-              name: doc.name,
-              height: doc.height,
-              weight: doc.weight,
-              types: doc.types,
-            };
-          })
-          .reverse()
-      );
-    } else console.error('error to get pokemons');
-  });
+export const searchPokemons: Handler = async (req, res) => {
+  const pokemons = await PokemonsModel.find();
+
+  res.send(pokemons);
 };
 
 export const getPokemon: Handler = async (req, res) => {
